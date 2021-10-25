6f0d74eddbde9954b1874354b2fb496210720fe0
noor <noormuhammadmalik95@gmail.com>
2021-10-25 13:41:43 +0500

    Initial commit of project

---
+++ src/fetch.py
@@ -0,0 +1,13 @@
+"""
+This CLI program fetches URLs and caches them locally in the current directory.
+You can view these links offline, since all assets are archived.
+
+Additionally, it gathers metadata on the fetched content and can provide that
+information if invoked with the --meta-data switch.
+
+It can also fetch multiple links at a time.
+
+Author: Noor Muhammad Malik
+Date: Oct 25, 2021
+License: None
+"""
